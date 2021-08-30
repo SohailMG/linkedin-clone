e0a6acf44c82dd6bbe2a16b804ce18949037d9de
Sohail Gsais <Gsais.sohail9@gmail.com>
2021-08-30 16:29:38 +0100

    added top section of feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create'
+import ImageIcon from '@material-ui/icons/Image'
+import InputOption from './InputOption'
+import { CalendarViewDay, EventNote, Subscriptions } from '@material-ui/icons'
 
 const Feed = () => {
     return (
@@ -13,7 +16,15 @@ const Feed = () => {
                         <button type="submit">send</button>
                     </form>
                 </div>
+                <div className="feed-inputOptions">
+                    <InputOption title="Photo" Icon={ImageIcon} color="#70B5F9"/>
+                    <InputOption title="Video" Icon={Subscriptions} color="orange"/>
+                    <InputOption title="Event" Icon={EventNote} color="red"/>
+                    <InputOption title="Write article" Icon={CalendarViewDay} color="green"/>
+                </div>
             </div>
+            {/* POSTS */}
+            {/* TODO: CREATE Post.js & Post.css */}
             
         </div>
     )
